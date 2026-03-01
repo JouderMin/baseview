8c080a62d9800d6642d2d565cab15056561c4475
JouderMin <joudermin@foxmail.com>
2026-03-01 18:27:29 +0800

    fix

--- src/macos/window.rs
+++ src/macos/window.rs
@@ -157,9 +157,9 @@ impl<'a> Window<'a> {
             println!("2");
             let child_window: id = msg_send![class!(NSWindow), alloc];
             println!("3");
-            let _: id = msg_send![child_window, setContentView: &ns_view];
+            let _: id = msg_send![child_window, setContentView: ns_view];
             println!("4");
-            let _: id = msg_send![parenrt_window, addChildWindow:&child_window ordered:NSWindowOrderingMode::NSWindowAbove];
+            let _: id = msg_send![parenrt_window, addChildWindow:child_window ordered:NSWindowOrderingMode::NSWindowAbove];
             println!("5");
         }
 
