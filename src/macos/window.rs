9fd5070e2410df443eff9ed8aac376a73500d639
JouderMin <joudermin@foxmail.com>
2026-03-01 19:31:48 +0800

    fix

--- src/macos/window.rs
+++ src/macos/window.rs
@@ -163,13 +163,15 @@ impl<'a> Window<'a> {
                 .map(|gl_config| Self::create_gl_context(None, ns_view, gl_config)),
         };
 
-        unsafe {
+        let window_handle = unsafe {
             let _: id = msg_send![handle.ns_view as *mut Object, addSubview: ns_view];
 
             let window_handle = Self::init(window_inner, window_info, build);
 
             let () = msg_send![pool, drain];
-        }
+
+            window_handle
+        };
 
         window_handle
     }
